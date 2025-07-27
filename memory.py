fb09234ba85821fe5ff125867777bd20c77dc726
Cyberlordrha <109386249+Cyberlordrha@users.noreply.github.com>
2025-07-27 03:06:17 +0100

    Update memory.py

--- memory.py
+++ memory.py
@@ -2,18 +2,28 @@ import json
 import os
 
 class MemoryManager:
-    MEMORY_FILE = 'last_session.json'
+    MEMORY_FILE = 'last_sessions.json'
 
     @staticmethod
     def save_session(data):
         """Save the session data to a JSON file."""
+        sessions = MemoryManager.load_sessions()
+        sessions.append(data)  # Append the new session
         with open(MemoryManager.MEMORY_FILE, 'w') as f:
-            json.dump(data, f)
+            json.dump(sessions, f)
 
     @staticmethod
-    def load_last_session():
-        """Load the last session data from a JSON file."""
+    def load_sessions():
+        """Load all session data from a JSON file."""
         if os.path.exists(MemoryManager.MEMORY_FILE):
             with open(MemoryManager.MEMORY_FILE, 'r') as f:
                 return json.load(f)
+        return []
+    
+    @staticmethod
+    def load_last_session():
+        """Load the last session data from a JSON file."""
+        sessions = MemoryManager.load_sessions()
+        if sessions:
+            return sessions[-1]  # Return the most recent session
         return None
